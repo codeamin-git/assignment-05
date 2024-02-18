f3039d33f9c13f2f19482b10affcec1760826c9e
codeamin11 <legacyofalamin@gmail.com>
2024-02-18 22:37:23 +0600

    seat background color added on clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
 // buy now button scroll to ticketing section
 function scrollButton(){
-    getElementId('scrollToTicket');
-    const scrollToTickets = getElementId('scrollToTickets');
+    getElement('scrollToTicket');
+    const scrollToTickets = getElement('scrollToTickets');
     scrollToTickets.scrollIntoView({behavior: 'smooth'});
 }
+
+selectASeat('A2');
+selectASeat('A3');
+selectASeat('A4');
+selectASeat('B1');
+selectASeat('A1');
+selectASeat('B2');
+selectASeat('B3');
+selectASeat('B4');
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,5 +1,16 @@
-function getElementId(elementId){
+function getElement(elementId){
     const element = document.getElementById(elementId);
-    console.log(element);
     return element;
+}
+
+function setBackgroundColorById(elementId){
+    elementId.classList.add('bg-[#1DD100]');
+
+}
+function selectASeat(seatId){
+    const seatNo = getElement(seatId);
+    seatNo.addEventListener('click', function(){
+        setBackgroundColorById(seatNo);
+    })
+
 }
